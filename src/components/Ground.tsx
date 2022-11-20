3898536e10b86b73bdb644387ddc9b0f38f26169
Elberd Chakhkiev <114188050+eckeriaue@users.noreply.github.com>
2022-11-20 19:21:48 +0300

    BUG

--- src/components/Ground.tsx
+++ src/components/Ground.tsx
@@ -1,18 +1,30 @@
 import { usePlane } from '@react-three/cannon'
 import { groundTexture } from '../img/textures'
 import { RepeatWrapping, NearestFilter } from 'three'
+import { useStore } from '../hooks/useStore'
+
 export const Ground = (): JSX.Element => {
   const [ref] = usePlane(() => ({
     rotation: [ -Math.PI / 2 , 0 ,0 ],
     position: [ 0, 0, 0 ],
   }))
+  
+  const [addCube] = useStore((state: any) => [state.addCube])
+
   groundTexture.magFilter = NearestFilter
   groundTexture.wrapS = RepeatWrapping
   groundTexture.wrapT = RepeatWrapping
   groundTexture.repeat.set(100, 100)
   
   return(
-    <mesh ref={ref}>
+    <mesh
+      onClick={(e) => {
+        e.stopPropagation()
+        const [x, y, z] = Object.values(e.point).map(val => Math.ceil(val))
+        addCube(x, y, z)
+      }}
+      ref={ref}
+    >
       <planeBufferGeometry
         attach='geometry'
         args={[100, 100]}
--- src/hooks/useStore.ts
+++ src/hooks/useStore.ts
@@ -2,34 +2,19 @@ import create from 'zustand'
 import { nanoid } from 'nanoid'
 export const useStore = create(set => ({
   texture: 'dirt',
-  cubes: [
-    {
-      key: nanoid(),
-      pos: [1,.5,1],
-      texture: 'dirt'
-    },
-    {
-      key: nanoid(),
-      pos: [2,.5,1],
-      texture: 'glass'
-    },
-  ],
-  addCube: (
-    x:number,
-    y:number,
-    z:number
-    ) => {
-      set((prev: any) => {
-        cubes: [
-          ...prev.cubes,
-          {
-            key: nanoid(),
-            pos: x,y,z,
-            texture: prev.texture 
-          }
-        ]
-      })
-    },
+  cubes: [],
+	addCube: (x:number, y:number, z:number) => {
+		set((prev: { cubes: any; texture: any }) => ({
+			cubes: [
+				...prev.cubes,
+				{
+					key: nanoid(),
+					pos: [x, y, z],
+					texture: prev.texture
+				}
+			]
+		}))
+	},
   removeCube: () => {},
   saveWorld: () => {},
   resetWorld: () => {},
