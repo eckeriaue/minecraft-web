b222c12441ae188392d3c1f7ed8adcd12bbda1be
Elberd Chakhkiev <114188050+eckeriaue@users.noreply.github.com>
2022-11-20 16:55:37 +0300

    player has  walking

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -9,16 +9,32 @@ import { useSphere } from '@react-three/cannon'
 import { type Object3D, Vector3 } from 'three'
 import { useEffect, useRef } from 'react'
 import { useKeyboard } from '../hooks/useKeyboard'
+
+
+const JUMP_FORCE = 3
+const SPEED = 4
 export const Player = (): JSX.Element => {
   const {camera} = useThree<RootState>() 
   const [ref, api] = useSphere<Object3D<Event>>(() => ({
     mass: 1,
     type: "Dynamic",
-    position: [0, 3, 0]
+    position: [0, 1, 0]
   }))
 
-  const actions = useKeyboard()
-  console.log('action', Object.entries(actions).filter(([e,v]) => v))
+  const {
+    moveForward,
+    moveBackward,
+    moveLeft,
+    moveRight,
+    jump
+  } = useKeyboard()
+  // console.log('action',
+  //   moveForward,
+  //   moveBackward,
+  //   moveLeft,
+  //   moveRight,
+  //   jump
+  // )
 
 
   const vel: React.MutableRefObject<number[]> = useRef<number[]>([0,0,0])
@@ -37,8 +53,32 @@ export const Player = (): JSX.Element => {
 
 
   useFrame(() => {
-    camera.position.copy(new Vector3(pos.current[0], pos.current[1], pos.current[3]))
-    // api.velocity.set(0,1,0)
+    camera.position.copy(new Vector3(pos.current[0], pos.current[1], pos.current[2]))
+    
+    const direction = new Vector3()
+
+    const frontVector = new Vector3(
+      0,
+      0,
+      (moveBackward ? 1 : 0) - (moveForward ? 1 : 0),
+    )
+    const sideVector = new  Vector3(
+      (moveLeft ? 1 : 0) - (moveRight ? 1 : 0),
+      0,
+      0,
+    )
+
+    direction
+      .subVectors(frontVector, sideVector)
+      .normalize()
+      .multiplyScalar(SPEED)
+      .applyEuler(camera.rotation)
+
+      api.velocity.set(direction.x, vel.current[1], direction.z)
+
+    if (jump && Math.abs(vel.current[1] < .05)) {
+      api.velocity.set(vel.current[0] , JUMP_FORCE ,vel.current[2])
+    }
   })
   return (
     <mesh ref={ref}>
